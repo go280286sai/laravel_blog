58b9fbad7c8c51e31877a7c5711bc650685e161a
Alexander Storchak <go280286sai@gmail.com>
2022-12-19 14:45:29 +0200

    fix comments

--- resources/views/admin/comments/index.blade.php
+++ resources/views/admin/comments/index.blade.php
@@ -1,27 +1,8 @@
 @extends('admin.layouts')
 
 @section('style')
-    <!-- Bootstrap 3.3.6 -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/bootstrap/css/bootstrap.min.css'}}">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/font-awesome/4.5.0/css/font-awesome.min.css'}}">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/ionicons/2.0.1/css/ionicons.min.css'}}">
     <!-- DataTables -->
     <link rel="stylesheet" href="{{env('APP_URL').'/assets/plugins/datatables/dataTables.bootstrap.css'}}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/dist/css/AdminLTE.min.css'}}">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/dist/css/skins/_all-skins.min.css'}}">
-    <!-- Button style-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        /* Darker background on mouse-over */
-        .btn:hover {
-            background-color: #8aa4af;
-        }
-    </style>
 @endsection
 
 @section('text')
@@ -29,7 +10,7 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <h1>
-             {{__('admin.comments_list')}}
+                {{__('admin.comments_list')}}
             </h1>
         </section>
         <!-- Main content -->
@@ -62,53 +43,43 @@
                                 </td>
                                 <td>
                                     @if($comment->status == 1)
-                                        <form action="{{env('APP_URL').'/admin/comments/toggle/'.$comment->id}}" method="get">
-                                            <button type="submit" >
+                                        <form action="{{env('APP_URL').'/admin/comments/toggle/'.$comment->id}}"
+                                              method="get">
+                                            <button type="submit">
                                                 <i class="fa fa-unlock"></i>
                                             </button>
                                         </form>
                                     @else
-                                        <form action="{{env('APP_URL').'/admin/comments/toggle/'.$comment->id}}" method="get">
-                                            <button type="submit" >
+                                        <form action="{{env('APP_URL').'/admin/comments/toggle/'.$comment->id}}"
+                                              method="get">
+                                            <button type="submit">
                                                 <i class="fa fa-lock"></i>
                                             </button>
                                         </form>
                                     @endif
                                     {{Form::open(['route'=>['comments.destroy', $comment->id], 'method'=>'delete'])}}
-                                    <button onclick="return confirm('{{__('admin.are_you_sure')}}')" type="submit" class="delete">
+                                    <button title="{{__('admin.delete')}}"
+                                            onclick="return confirm('{{__('admin.are_you_sure')}}')" type="submit"
+                                            class="delete">
                                         <i class="fa fa-trash"></i>
                                     </button>
                                 {{Form::close()}}
                             </tr>
                         @endforeach
-                        </tfoot>
                     </table>
                 </div>
                 <!-- /.box-body -->
             </div>
             <!-- /.box -->
-
         </section>
         <!-- /.content -->
     </div>
 @endsection
 
 @section('js')
-    <!-- jQuery 2.2.3 -->
-    <script src="{{env('APP_URL').'/assets/plugins/jQuery/jquery-2.2.3.min.js'}}"></script>
-    <!-- Bootstrap 3.3.6 -->
-    <script src="{{env('APP_URL').'/assets/bootstrap/js/bootstrap.min.js'}}"></script>
     <!-- DataTables -->
     <script src="{{env('APP_URL').'/assets/plugins/datatables/jquery.dataTables.min.js'}}"></script>
     <script src="{{env('APP_URL').'/assets/plugins/datatables/dataTables.bootstrap.min.js'}}"></script>
-    <!-- SlimScroll -->
-    <script src="{{env('APP_URL').'/assets/plugins/slimScroll/jquery.slimscroll.min.js'}}"></script>
-    <!-- FastClick -->
-    <script src="{{env('APP_URL').'/assets/plugins/fastclick/fastclick.js'}}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{env('APP_URL').'/assets/dist/js/app.min.js'}}"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{env('APP_URL').'/assets/dist/js/demo.js'}}"></script>
     <!-- page script -->
     <script>
         $(function () {
--- resources/views/admin/comments/trash.blade.php
+++ resources/views/admin/comments/trash.blade.php
@@ -1,26 +1,8 @@
 @extends('admin.layouts')
 
 @section('style')
-    <!-- Bootstrap 3.3.6 -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/bootstrap/css/bootstrap.min.css'}}">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/font-awesome/4.5.0/css/font-awesome.min.css'}}">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/ionicons/2.0.1/css/ionicons.min.css'}}">
     <!-- DataTables -->
     <link rel="stylesheet" href="{{env('APP_URL').'/assets/plugins/datatables/dataTables.bootstrap.css'}}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/dist/css/AdminLTE.min.css'}}">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/dist/css/skins/_all-skins.min.css'}}">
-    <!-- Button style-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        .btn:hover {
-            background-color: #8aa4af;
-        }
-    </style>
 @endsection
 
 @section('text')
@@ -49,13 +31,12 @@
                                                 class="btn btn-success"><i>{{__('admin.recovery_all')}}</i></button>
                                     </form>
                                 </td>
-                                <td> |</td>
                                 <td>
                                     <form action="{{env('APP_URL').'/admin/comments_recover'}}" method="post">
                                         @csrf
                                         <input type="hidden" name="target" value="trash_all">
                                         <button title="{{__('admin.delete_all_comments')}}?" onclick="return confirm('{{__('admin.are_you_sure')}}')"
-                                                class="btn btn-success"><i>{{__('admin.delete_all')}}</i></button>
+                                                class="btn btn-success btn_left"><i>{{__('admin.delete_all')}}</i></button>
                                     </form>
                                 </td>
                             </tr>
@@ -74,7 +55,6 @@
                         </tr>
                         </thead>
                         <tbody>
-
                         @foreach($trash as $item)
                             @if($item->post->title??false)
                             <tr>
@@ -110,7 +90,6 @@
                             </tr>
                         @endif
                         @endforeach
-                        </tfoot>
                     </table>
                 </div>
                 <div class="box-footer">
@@ -119,7 +98,6 @@
                 <!-- /.box-body -->
             </div>
             <!-- /.box -->
-
         </section>
         <!-- /.content -->
     </div>
@@ -127,21 +105,9 @@
 @endsection
 
 @section('js')
-    <!-- jQuery 2.2.3 -->
-    <script src="{{env('APP_URL').'/assets/plugins/jQuery/jquery-2.2.3.min.js'}}"></script>
-    <!-- Bootstrap 3.3.6 -->
-    <script src="{{env('APP_URL').'/assets/bootstrap/js/bootstrap.min.js'}}"></script>
     <!-- DataTables -->
     <script src="{{env('APP_URL').'/assets/plugins/datatables/jquery.dataTables.min.js'}}"></script>
     <script src="{{env('APP_URL').'/assets/plugins/datatables/dataTables.bootstrap.min.js'}}"></script>
-    <!-- SlimScroll -->
-    <script src="{{env('APP_URL').'/assets/plugins/slimScroll/jquery.slimscroll.min.js'}}"></script>
-    <!-- FastClick -->
-    <script src="{{env('APP_URL').'/assets/plugins/fastclick/fastclick.js'}}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{env('APP_URL').'/assets/dist/js/app.min.js'}}"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{env('APP_URL').'/assets/dist/js/demo.js'}}"></script>
     <!-- page script -->
     <script>
         $(function () {
