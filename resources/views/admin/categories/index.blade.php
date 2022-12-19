60ff6c23f6fd3d4a1dd3fffeb8ab27be3bb5c929
Alexander Storchak <go280286sai@gmail.com>
2022-12-19 14:27:05 +0200

    fix category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,27 +1,10 @@
 @extends('admin.layouts')
+
 @section('style')
-    <!-- Bootstrap 3.3.6 -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/bootstrap/css/bootstrap.min.css'}}">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/font-awesome/4.5.0/css/font-awesome.min.css'}}">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/ionicons/2.0.1/css/ionicons.min.css'}}">
     <!-- DataTables -->
     <link rel="stylesheet" href="{{env('APP_URL').'/assets/plugins/datatables/dataTables.bootstrap.css'}}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/dist/css/AdminLTE.min.css'}}">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="{{env('APP_URL').'/assets/dist/css/skins/_all-skins.min.css'}}">
-    <!-- Button style-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        /* Darker background on mouse-over */
-        .btn:hover {
-            background-color: #8aa4af;
-        }
-    </style>
 @endsection
+
 @section('text')
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
@@ -57,44 +40,30 @@
                                     <form action="{{env('APP_URL').'/admin/categories/'.$category->id.'/edit/'}}"
                                           method="get">
                                         @csrf
-                                        <button class="btn"><i class="fa fa-bars"></i></button>
+                                        <button class="btn" title="{{__('admin.edit')}}"><i class="fa fa-bars"></i></button>
                                     </form>
                                     <form action="{{env('APP_URL').'/admin/categories/'.$category->id}}" method="post">
                                         @method('delete')
                                         @csrf
-                                        <button onclick="return confirm('{{__('admin.are_you_sure')}}')" class="btn"><i class="fa fa-trash"></i></button>
+                                        <button onclick="return confirm('{{__('admin.are_you_sure')}}')" class="btn" title="{{__('admin.delete')}}"><i class="fa fa-trash"></i></button>
                                     </form>
                                 </td>
                             </tr>
                         @endforeach
-                        </tfoot>
                     </table>
                 </div>
                 <!-- /.box-body -->
             </div>
             <!-- /.box -->
-
         </section>
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
 @endsection
 @section('js')
-    <!-- jQuery 2.2.3 -->
-    <script src="{{env('APP_URL').'/assets/plugins/jQuery/jquery-2.2.3.min.js'}}"></script>
-    <!-- Bootstrap 3.3.6 -->
-    <script src="{{env('APP_URL').'/assets/bootstrap/js/bootstrap.min.js'}}"></script>
     <!-- DataTables -->
     <script src="{{env('APP_URL').'/assets/plugins/datatables/jquery.dataTables.min.js'}}"></script>
     <script src="{{env('APP_URL').'/assets/plugins/datatables/dataTables.bootstrap.min.js'}}"></script>
-    <!-- SlimScroll -->
-    <script src="{{env('APP_URL').'/assets/plugins/slimScroll/jquery.slimscroll.min.js'}}"></script>
-    <!-- FastClick -->
-    <script src="{{env('APP_URL').'/assets/plugins/fastclick/fastclick.js'}}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{env('APP_URL').'/assets/dist/js/app.min.js'}}"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{env('APP_URL').'/assets/dist/js/demo.js'}}"></script>
     <!-- page script -->
     <script>
         $(function () {
