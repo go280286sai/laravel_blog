b797782c9e6a8e1af0fa688e1248758101685134
Sergey Kudashev <kudashevs@gmail.com>
2023-04-02 20:38:05 +0300

    update the ShareButtons usage

--- resources/views/pages/social.blade.php
+++ resources/views/pages/social.blade.php
@@ -1,14 +1,18 @@
 <div class="social-share">
     <span class="social-share-title pull-left text-capitalize">By {{$post->user->name}} On <strong
             class="red">{{$post->getDate()}}</strong> </span>
-    <ul class="text-center pull-right">
-        <li>{!! ShareButtons::page(route('post.show', $post->slug), $post->title)->facebook() !!}</li>
-        <li>{!! ShareButtons::page(route('post.show', $post->slug), $post->title)->twitter() !!}</li>
-        <li>{!! ShareButtons::page(route('post.show', $post->slug), $post->title)->linkedin() !!}</li>
-        <li>{!! ShareButtons::page(route('post.show', $post->slug), $post->title)->telegram() !!}</li>
-        <li>{!! ShareButtons::page(route('post.show', $post->slug), $post->title)->whatsapp() !!}</li>
-        <li>{!! ShareButtons::page(route('post.show', $post->slug), $post->title)->skype() !!}</li>
-        <li>{!! ShareButtons::page(route('post.show', $post->slug), $post->title)->copylink() !!}</li>
-        <li>{!! ShareButtons::page(route('post.show', $post->slug), $post->title)->mailto() !!}</li>
-    </ul>
+            {!! ShareButtons::page(route('post.show', $post->slug), $post->title, [
+                    'block_prefix' => '<ul class="text-center pull-right">',
+                    'block_suffix' => '</ul>',
+                    'element_prefix' => '<li>',
+                    'element_suffix' => '</li>',
+               ])->facebook()
+                 ->twitter()
+                 ->linkedin()
+                 ->telegram()
+                 ->whatsapp()
+                 ->skype()
+                 ->copylink()
+                 ->mailto()
+            !!}
 </div>
